69cb2380d749bcb3e2a0c52bcc4f75c0fda88e61
Milagro Teruel <milagro.teruel@gmail.com>
2018-02-23 16:07:55 -0300

    Updating base dkt run script

--- run_dkt.py
+++ run_dkt.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-import json
 import utils
+import tensorflow as tf
 
 from models import dkt
 from quick_experiment import dataset
@@ -14,8 +14,8 @@ def parse_arguments():
     parser.add_argument('--filename', type=str,
                         help='The path to the pickled file with the processed'
                              'sequences.')
-    parser.add_argument('--test_predictions_filename', type=str,
-                        help='The path to the file to store the predictions')
+    parser.add_argument('--test_prediction_dir', type=str,
+                        help='The path to a directory to store the predictions')
     parser.add_argument('--training_epochs', type=int, default=500,
                         help='Number of epochs to run.')
     parser.add_argument('--hidden_layer_size', type=int, default=100,
@@ -30,7 +30,11 @@ def parse_arguments():
     parser.add_argument('--dropout_ratio', type=float, default=0.3,
                         help='Dropout for the input layer and the recurrent '
                              'layer.')
-
+    parser.add_argument('--learning_rate', type=float, default=0.001,
+                        help='Learning rate.')
+    parser.add_argument('--runs', type=int, default=1,
+                        help='Number of times to run the experiment with'
+                             'different samples')
 
     return parser.parse_args()
 
@@ -54,7 +58,7 @@ def read_configuration(args):
         'logs_dirname': args.logs_dirname,
         'log_values': args.log_values,
         'max_num_steps': args.max_num_steps,
-
+        'learning_rate': args.learning_rate,
     }
     dataset_config = {'train': 0.7, 'test': 0.2, 'validation': 0.1}
     return config, dataset_config
@@ -77,16 +81,33 @@ def main():
     print(partitions)
     print('Experiment Configuration')
     print(experiment_config)
-    model = dkt.DktLSTMModel(assistment_dataset, **experiment_config)
-    model.fit(partition_name='train', close_session=False,
-              training_epochs=args.training_epochs)
-    predicted_labels = model.predict('test')
-    utils.pickle_to_file(
-        predicted_labels,
-        os.path.join(args.test_predictions_filename, 'predictions.p'))
-    utils.pickle_to_file(
-        (model.training_performance, model.validation_performance),
-        os.path.join(args.test_predictions_filename, 'performances.p'))
+
+    # Check all directories exist
+    if args.base_logs_dirname:
+        utils.safe_mkdir(args.base_logs_dirname)
+    utils.safe_mkdir(args.test_prediction_dir)
+
+    for run in range(args.runs):
+        print('Running iteration {} of {}'.format(run + 1, args.runs))
+        assistment_dataset.set_current_sample(run)
+        if args.base_logs_dirname:
+            tf.reset_default_graph()
+            logs_dirname = os.path.join(args.base_logs_dirname,
+                                        'run{}'.format(run))
+            utils.safe_mkdir(logs_dirname)
+            experiment_config['logs_dirname'] = logs_dirname
+        model = dkt.DktLSTMModel(assistment_dataset, **experiment_config)
+        model.fit(partition_name='train', close_session=False,
+                  training_epochs=args.training_epochs)
+        predicted_labels = model.predict('test')
+        model.sess.close()
+        prediction_dirname = os.path.join(
+            args.test_prediction_dir, 'predictions_run{}.p'.format(run))
+        utils.pickle_to_file(predicted_labels, prediction_dirname)
+        utils.pickle_to_file(
+            (model.training_performance, model.validation_performance),
+            os.path.join(args.test_prediction_dir,
+                         'performances_run{}.p'.format(run)))
 
 
 if __name__ == '__main__':
